8690a6a8c8cd4369b1045568aaa52048ec7b6103
atlfinesse <finesse.cap@gmail.com>
2021-02-13 11:51:15 -0800

    Created to do list

--- typescript-template/src/App.tsx
+++ typescript-template/src/App.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-    const sum = (a:number, b:number):number => a + b
+type FormElem = React.FormEvent<HTMLFormElement>
+interface ITodo {
+    text: string;
+    complete: boolean;
+}
+
+
+function App():JSX.Element {
+    const [value, setValue] = useState<string>('')
+    const [todos, setTodos] = useState<ITodo[]>([])
+    
+    const addTodo = (text:string):void => {
+        const newTodos: ITodo[] = [...todos, {text, complete: false}]
+        setTodos(newTodos)
+    }
+    
+    const handleSubmit = (e:FormElem):void => {
+        e.preventDefault()
+        addTodo(value)
+        setValue('')
+    }
+
+    const completeTodo = (index: number):void => {
+        const newTodos: ITodo[] = [...todos]
+        newTodos[index].complete = !newTodos[index].complete
+        setTodos(newTodos)
+    }
 
     return (
     <div className="App">
-        <h1>Hello</h1>
-        <p>{sum(5,12)}</p>
+        <h1>To Do List</h1>
+        <form onSubmit={handleSubmit}>
+            <input type='text' value={value} required onChange={e => setValue(e.target.value)}/>
+            <button type='submit'>Add To do</button>
+        </form>
+        <div>
+            {todos.map((todo: ITodo, index: number) => {
+                return (
+                    <div key={index}>
+                        <p>{todo.text}</p>
+                        <button onClick={() => completeTodo(index)}>
+                            {todo.complete ? 'Incomplete' : 'Complete'}
+                        </button>
+                    </div>
+                )
+            })}
+        </div>
     </div>
     );
 }
